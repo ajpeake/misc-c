c8a10ed60307c2f273a65a2fbbbec6e26c175516
Art Peake <ajpeake@gmail.com>
2018-07-19 17:13:02 -0500

    minor changes to zeroit.c

--- zeroit.c
+++ zeroit.c
@@ -8,19 +8,6 @@
 int main(int argc, char * argv[])
 {
 
-	// A function that terminates when enter key is pressed
-	void fun()
-	{
-		printf("fun() starts \n");
-		printf("Press enter to stop fun \n");
-		while(1)
-    		{
-			if (getchar())
-			break;
-		}
-		printf("fun() ends \n");
-	}
- 
 	int fd;		// File Descriptor
 	clock_t t;	// Variables for timing of code
 
@@ -57,11 +44,9 @@ int main(int argc, char * argv[])
 
 	close(fd);
 
-	fun();
-
 	t = clock() - t;
 	double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-	printf("fun() took %f seconds to execute \n", time_taken);
+	printf("This program took %f seconds to execute \n\n", time_taken);
 
 	return 0;
 }
