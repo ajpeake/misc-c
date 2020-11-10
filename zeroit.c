9781b5476e8c5975e455a2b4c4f63d46621433d0
Art Peake <ajpeake@gmail.com>
2020-11-09 20:52:27 -0600

    added comment and reformatted code

--- zeroit.c
+++ zeroit.c
@@ -5,35 +5,39 @@
 #include <unistd.h>
 #include <time.h>
 
-int main(int argc, char * argv[])
+//
+// Adding a change to this file for testing purposes
+//
+
+int main(int argc, char *argv[])
 {
 
-	int fd;		// File Descriptor
-	clock_t t;	// Variables for timing of code
+	int fd;	   // File Descriptor
+	clock_t t; // Variables for timing of code
 
-	t = clock();	// Take the start time for timing.	
+	t = clock(); // Take the start time for timing.
 
 	// Check to see if we have a file to work with. If not, then bail.
 
 	if (argc < 2)
 	{
-	  printf("\nPlease specify a file to truncate!\n\n");
-	  return -1;
+		printf("\nPlease specify a file to truncate!\n\n");
+		return -1;
 	}
 
 	// Open file specified on the command line
 	// Open as read/write and truncate the file
 
-	fd = open(argv[1], O_RDWR|O_TRUNC);
+	fd = open(argv[1], O_RDWR | O_TRUNC);
 
 	// If for some reason the file open is in error, print a message and then bail.
 
 	if (fd < 0)
 	{
-	  printf("\nFile does not exist!\n");
-	  printf("\nFile Descriptor %d\n\n", fd);
-	  close(fd);
-	  return -1;
+		printf("\nFile does not exist!\n");
+		printf("\nFile Descriptor %d\n\n", fd);
+		close(fd);
+		return -1;
 	}
 
 	// Start to do work here since the file is open.
@@ -45,7 +49,7 @@ int main(int argc, char * argv[])
 	close(fd);
 
 	t = clock() - t;
-	double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+	double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
 	printf("This program took %f seconds to execute \n\n", time_taken);
 
 	return 0;
